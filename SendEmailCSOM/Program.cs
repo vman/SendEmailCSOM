86bdb2106979ce3203e85bd196c8a5b4329a3f5f
Vardhaman Deshpande <vardhaman.rd@gmail.com>
2014-08-25 18:34:14 +0100

    added code to send email to a user in the same tenant

--- SendEmailCSOM/Program.cs
+++ SendEmailCSOM/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,36 @@ namespace SendEmailCSOM
     {
         static void Main(string[] args)
         {
+            using (ClientContext clientContext = new ClientContext("https://yoursite.sharepoint.com/"))
+            {
+                SecureString passWord = new SecureString();
+
+                foreach (char c in "password".ToCharArray()) passWord.AppendChar(c);
+
+                clientContext.Credentials = new SharePointOnlineCredentials("loginname@yoursite.onmicrosoft.com", passWord);
+
+                EmailProperties emailProps = new EmailProperties();
+
+                List<string> toList = new List<string>() { "user@yoursite.onmicrosoft.com" };
+                
+                emailProps.To = toList;
+
+                emailProps.From = "loginname@yoursite.onmicrosoft.com";
+                
+                emailProps.Body = "Body of the email";
+                
+                emailProps.Subject = "Email from CSOM";
+
+                Microsoft.SharePoint.Client.Utilities.Utility.SendEmail(clientContext, emailProps);
+
+                Console.WriteLine("Executing Query...");
+
+                clientContext.ExecuteQuery();
+
+                Console.WriteLine("done");
+
+                Console.ReadLine();
+            }
         }
     }
 }
